304da3d7b9a3c2d51b6003f0bd083bb79ebd9809
phcbarros <paulo.barros@outlook.com.br>
2020-06-30 20:03:17 -0300

    Curso Alura - Swift 1 - 02 Aprendendo Swift no playground - 03 Funcoes sem e com parâmetros e invocando funcoes

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,3 +10,25 @@ let nome = "pastel"
 let felicidade: Int = 5
 let calorias: Double = 80.7
 let gostou: Bool = true
+
+//func comer() {
+//    print("comeu o \(nome)")
+//}
+
+//comer()
+
+// função com parâmetros explícitos
+//func comeu(nome: String, calorias: Double) {
+//    print("comeu \(nome) que tinha \(calorias) calorias")
+//}
+//
+//comeu(nome: nome, calorias: calorias)
+
+// função com parâmetros implicítos
+// usar o _ + espace antes do nome da variável possibilita oculta-la na chamada do método
+func comeu(_ nome: String, _ calorias: Double) {
+    print("comeu \(nome) que tinha \(calorias) calorias")
+}
+
+comeu(nome, calorias)
+
