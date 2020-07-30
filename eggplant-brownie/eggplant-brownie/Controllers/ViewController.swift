35f8e792e20ac454a2099e28439543456cbaa824
MAC-00055 <MAC-00055@Arkloks-MacBook-Pro.local>
2020-07-30 20:54:23 -0300

    Curso Alura - Swift 2 - 03 - Compartilhando objetos entre controllers - 02 Push com o storyboard e Pop programático

--- eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
@@ -37,7 +37,8 @@ class ViewController: UIViewController {
         
         let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
         print("comi \(refeicao.nome) e fiquei com felicidade \(refeicao.felicidade)")
-
+        
+        navigationController?.popViewController(animated: true)
     }
     
 }
