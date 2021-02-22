8abb428b805999290bd3b521f4c3db2270dd52fa
phcbarros <paulo.barros@outlook.com.br>
2021-02-22 15:55:36 -0300

    Curso Alura - Swift 2 - 04 Definindo identificadores para os segue - 03 Definindo identificadores para os segue

--- eggplant-brownie/eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -17,26 +17,31 @@ class RefeicoesTableViewController : UITableViewController {
         Refeicao(nome: "Yakisoba", felicidade: 4)
     ]
     
+    // número de linhas da tabela - obrigatório implementar esse método
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return refeicoes.count
     }
     
+    // para cada número de linhas da tabela esse método será executado - obrigatório implementar esse método
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
-        let refeicao = refeicoes[indexPath.row]
+        let refeicao = refeicoes[indexPath.row] // posição da linha que a tableView está executando
         celula.textLabel?.text = refeicao.nome
         
         return celula
     }
     
+    // por questões de legibilibidade usamos o _ para ocultar o parametro
     func add(_ refeicao: Refeicao) -> Void {
         refeicoes.append(refeicao)
         tableView.reloadData()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let viewController = segue.destination as? ViewController {
-            viewController.tableViewControler = self
+        if segue.identifier == "adicionar" {
+            if let viewController = segue.destination as? ViewController {
+                viewController.tableViewControler = self
+            }
         }
     }
 }
