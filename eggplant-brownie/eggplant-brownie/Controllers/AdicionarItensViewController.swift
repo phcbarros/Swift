8598195b5828113372df66614d2a2c6a13f4914b
phcbarros <paulo.barros@outlook.com.br>
2021-02-23 15:04:44 -0300

    Curso Alura - Swift 2 - 07 Problemas de herança e inicializadores - 01 Criando um xib

---
+++ eggplant-brownie/eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AdicionarItensViewController.swift
+//  eggplant-brownie
+//
+//  Created by Paulo Barros on 23/02/21.
+//  Copyright © 2021 Paulo Barros. All rights reserved.
+//
+
+import UIKit
+
+class AdicionarItensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
