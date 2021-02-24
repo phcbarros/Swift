4b2dea63b05f7d0f4769bd7d69684b839f950fe1
phcbarros <paulo.barros@outlook.com.br>
2021-02-24 15:36:11 -0300

    Curso Alura - Swift 3 - 06 Salvando e carregando imagens - 01 NSCoding, serialização e desserialização de objetos

--- eggplant-brownie/eggplant-brownie/Models/Refeicao.swift
+++ eggplant-brownie/eggplant-brownie/Models/Refeicao.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Refeicao: NSObject {
+class Refeicao: NSObject, NSCoding {
     
     // MARK: - Atributos
     
@@ -21,6 +21,18 @@ class Refeicao: NSObject {
         self.nome = nome;
         self.felicidade = felicidade
         self.itens = itens
+    }
+    
+    // MARK: - NSCoding
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(nome, forKey: "nome")
+        coder.encode(felicidade, forKey: "felicidade")
+        coder.encode(itens, forKey: "itens")
+    }
+    
+    required init?(coder: NSCoder) {
+        
     }
     
     // MARK: - Métodos
@@ -43,4 +55,7 @@ class Refeicao: NSObject {
         
         return mensagem
     }
+    
+    
+    
 }
