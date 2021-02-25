a19e156a2ffe487c54326d6eaed4d6d90945eb83
phcbarros <paulo.barros@outlook.com.br>
2021-02-25 13:48:18 -0300

    Curso Alura - iOS Layout 1 -  01 Iniciando a implementação do app "Alura Viagens" - 01 Introdução

---
+++ alura-viagens/alura-viagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alura-viagens
+//
+//  Created by Paulo Barros on 25/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
