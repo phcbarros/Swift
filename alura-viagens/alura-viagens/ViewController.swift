1e1c99da4690a5d6ed57a195f9469f0685643286
phcbarros <paulo.barros@outlook.com.br>
2021-02-25 14:31:56 -0300

    Curso Alura - iOS Layout 1 -  01 Iniciando a implementação do app "Alura Viagens" - 04 Implementando Table View

--- alura-viagens/alura-viagens/ViewController.swift
+++ alura-viagens/alura-viagens/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let viagens: [String] = ["Rio de Janeiro", "Ceará", "São Paulo"]
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var tabelaViagens: UITableView!
+    
+    // MARK: - Init
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // define que o esse controller (self) irá controlar a tabela
+        self.tabelaViagens.dataSource = self
     }
-
-
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celula", for: indexPath)
+        celula.textLabel?.text = viagens[indexPath.row]
+        return celula
+    }
+    
 }
 
